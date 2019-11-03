a06a05c380d91273c9c5202b16ce0429890f13f5
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-11-03 16:11:20 -0300

    Add sliders to cube story

--- stories/3-demo.stories.js
+++ stories/3-demo.stories.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import { Button } from '@storybook/react/demo';
-import { Scene, Plane, Roof, Floor, LWall, RWall } from '../.';
+import {
+  Scene,
+  Plane,
+  Roof,
+  Floor,
+  LWall,
+  RWall,
+  SceneContent,
+  SceneContainer,
+  RotateX,
+  RotateY,
+} from '../.';
 
 export default {
   title: 'Demo',
 };
 
-export const text = () => <HalfCube />;
-
-export const emoji = () => (
-  <Button onClick={action('clicked')}>
-    <span role="img" aria-label="so cool">
-      😀 😎 👍 💯
-    </span>
-  </Button>
-);
+export const cube = () => <HalfCube />;
 
 function HalfCube() {
   const side = 100;
@@ -24,101 +27,104 @@ function HalfCube() {
   const [z, setZ] = React.useState(0.5);
   return (
     <div>
-      <Scene
-        className="scene"
+      <SceneContainer
         style={{ height: window.innerHeight }}
-        perspective={side * 40}
+        perspective={side * 10}
         scale={2}
-        origin={{ x: window.innerWidth / 2, y: window.innerHeight / 2 }}
       >
-        {/* <Plane w={10} h={1} style={{ background: '#2229' }} />
-        <Plane w={1} h={10} style={{ background: '#2229' }} /> */}
-        <div
-          style={{
-            transformStyle: 'preserve-3d',
-            position: 'absolute',
-            transform: ' ',
-          }}
-        >
-          <div
-            style={{
-              position: 'absolute',
-              transformStyle: 'preserve-3d',
-              transform: 'rotate3d(0,1,0,-45deg) rotate3d(1,0,0,-45deg)',
-            }}
-          >
-            {/* Floor */}
-            <Plane
-              w={side}
-              h={side}
-              pinX="left"
-              pinY="top"
-              rotate={{ x: 1, y: 0, z: 0, a: 90 }}
-              style={{ background: '#717483', overflow: 'hidden' }}
-            >
-              <Slider
-                value={x}
-                setValue={setX}
-                style={{
-                  top: 0,
-                  transform: 'translateY(-50%)',
-                }}
-              />
-              <Slider
-                value={z}
-                setValue={setZ}
-                style={{
-                  top: 0,
-                  transformOrigin: 'left',
-                  transform: 'translateY(-50%) rotateZ(90deg)',
-                }}
-              />
-            </Plane>
-            {/* Left */}
-            <LWall
-              w={side}
-              h={side}
-              pinX="right"
-              pinY="bottom"
-              style={{ background: '#817473', overflow: 'hidden' }}
-            >
-              <Slider
-                value={z}
-                setValue={setZ}
-                style={{
-                  bottom: 0,
-                  transform: 'translateY(50%) rotateZ(180deg)',
-                }}
-              />
-            </LWall>
-            {/* Back */}
-            <Plane
-              w={side}
-              h={side}
-              pinX="left"
-              pinY="bottom"
-              style={{ background: '#718473', overflow: 'hidden' }}
-            >
-              <Slider
-                value={y}
-                setValue={setY}
-                style={{
-                  bottom: 0,
-                  transformOrigin: 'left',
-                  transform: 'translateY(50%) rotateZ(-90deg)',
-                }}
-              />
-            </Plane>
-            <Ball
-              r={side / 20}
-              x={x * side}
-              y={-y * side}
-              z={z * side}
-              style={{ background: '#fdb813' }}
-            ></Ball>
-          </div>
-        </div>
-      </Scene>
+        <SceneContent style={{ height: '100%', width: '100%' }}>
+          <RotateX degrees={-30}>
+            <RotateY degrees={-45}>
+              {/* Floor */}
+              <Plane
+                w={side}
+                h={side}
+                pinX="left"
+                pinY="top"
+                rotate={{ x: 1, y: 0, z: 0, a: 90 }}
+                style={{ background: '#717483', overflow: 'hidden' }}
+              >
+                <Slider
+                  value={x}
+                  setValue={setX}
+                  style={{
+                    top: 0,
+                    transform: 'translateY(-50%)',
+                  }}
+                />
+                <Slider
+                  value={z}
+                  setValue={setZ}
+                  style={{
+                    top: 0,
+                    transformOrigin: 'left',
+                    transform: 'translateY(-50%) rotateZ(90deg)',
+                  }}
+                />
+              </Plane>
+              {/* Left */}
+              <LWall
+                w={side}
+                h={side}
+                pinX="right"
+                pinY="bottom"
+                style={{ background: '#817473', overflow: 'hidden' }}
+              >
+                <Slider
+                  value={z}
+                  setValue={setZ}
+                  style={{
+                    bottom: 0,
+                    transform: 'translateY(50%) rotateZ(180deg)',
+                  }}
+                />
+                <Slider
+                  value={y}
+                  setValue={setY}
+                  style={{
+                    transformOrigin: 'right',
+                    top: 0,
+                    transform: 'translateY(-50%) rotateZ(-90deg)',
+                  }}
+                />
+              </LWall>
+              {/* Back */}
+              <Plane
+                w={side}
+                h={side}
+                pinX="left"
+                pinY="bottom"
+                style={{ background: '#718473', overflow: 'hidden' }}
+              >
+                <Slider
+                  value={y}
+                  setValue={setY}
+                  style={{
+                    bottom: 0,
+                    transformOrigin: 'left',
+                    transform: 'translateY(50%) rotateZ(-90deg)',
+                  }}
+                />
+                <Slider
+                  value={x}
+                  setValue={setX}
+                  style={{
+                    bottom: 0,
+                    transform: 'translateY(50%) ',
+                  }}
+                />
+              </Plane>
+              <Ball
+                r={side / 20}
+                x={x * side}
+                y={-y * side}
+                z={z * side}
+                style={{ background: '#fdb813' }}
+              ></Ball>
+            </RotateY>
+          </RotateX>
+        </SceneContent>
+      </SceneContainer>
     </div>
   );
 }
