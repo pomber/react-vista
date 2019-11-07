ea1eeadc648568a5baf638ca53c8782af7e8f2eb
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-11-07 14:40:49 -0300

    Add controls to cube

--- stories/0-demo.stories.js
+++ stories/0-demo.stories.js
@@ -29,140 +29,157 @@ function HalfCube() {
   const [leftColor, setLeftColor] = React.useState('#668866');
   const [floorColor, setFloorColor] = React.useState('#666688');
   const [lightColor, setLightColor] = React.useState('#FDB813');
+  const [angle, setAngle] = React.useState(-45);
   return (
-    <div>
-      <SceneContainer
-        style={{ height: window.innerHeight, color: '#fafafa' }}
-        perspective={side * 10}
-        scale={3}
-      >
-        <SceneContent style={{ height: '100%', width: '100%' }}>
-          <NoLights>
-            <Plane w={side} h={side / 4} y={-side} pinX="center" pinY="bottom">
-              Light Color:
+    <SceneContainer
+      style={{ height: window.innerHeight, color: '#fafafa' }}
+      perspective={side * 10}
+      scale={3}
+    >
+      <SceneContent style={{ height: '100%', width: '100%' }}>
+        <NoLights>
+          <Plane
+            w={side / 2}
+            h={side / 4}
+            y={-side}
+            pinX="center"
+            pinY="bottom"
+          >
+            <div>
+              Light
               <input
                 type="color"
-                style={{ marginLeft: '10px' }}
+                style={{ marginLeft: '46px' }}
                 value={lightColor}
                 onChange={e => setLightColor(e.target.value)}
               />
+            </div>
+
+            <input
+              type="range"
+              min={-90}
+              max={0}
+              step={1}
+              style={{ marginTop: '16px' }}
+              value={angle}
+              onChange={e => setAngle(+e.target.value)}
+            />
+          </Plane>
+        </NoLights>
+        <RotateX degrees={-30}>
+          <RotateY degrees={angle}>
+            {/* Floor */}
+            <Floor
+              w={side}
+              h={side}
+              pinX="left"
+              pinY="top"
+              style={{ background: floorColor, overflow: 'hidden' }}
+            >
+              <Slider
+                value={x}
+                setValue={setX}
+                style={{
+                  top: 0,
+                  transform: 'translateY(-50%)',
+                }}
+              />
+              <Slider
+                value={z}
+                setValue={setZ}
+                style={{
+                  top: 0,
+                  transformOrigin: 'left',
+                  transform: 'translateY(-50%) rotateZ(90deg)',
+                }}
+              />
+              <ColorPicker
+                color={floorColor}
+                setColor={setFloorColor}
+                style={{ bottom: '10%', right: '10%' }}
+              />
+            </Floor>
+            {/* Left */}
+            <LWall
+              w={side}
+              h={side}
+              pinX="right"
+              pinY="bottom"
+              style={{ background: leftColor, overflow: 'hidden' }}
+            >
+              <Slider
+                value={z}
+                setValue={setZ}
+                style={{
+                  bottom: 0,
+                  transform: 'translateY(50%) rotateZ(180deg)',
+                }}
+              />
+              <Slider
+                value={y}
+                setValue={setY}
+                style={{
+                  transformOrigin: 'right',
+                  top: 0,
+                  transform: 'translateY(-50%) rotateZ(-90deg)',
+                }}
+              />
+              <ColorPicker
+                color={leftColor}
+                setColor={setLeftColor}
+                style={{ top: '10%', left: '10%' }}
+              />
+            </LWall>
+            {/* Back */}
+            <Plane
+              w={side}
+              h={side}
+              pinX="left"
+              pinY="bottom"
+              style={{ background: backColor, overflow: 'hidden' }}
+            >
+              <Slider
+                value={y}
+                setValue={setY}
+                style={{
+                  bottom: 0,
+                  transformOrigin: 'left',
+                  transform: 'translateY(50%) rotateZ(-90deg)',
+                }}
+              />
+              <Slider
+                value={x}
+                setValue={setX}
+                style={{
+                  bottom: 0,
+                  transform: 'translateY(50%) ',
+                }}
+              />
+              <ColorPicker
+                color={backColor}
+                setColor={setBackColor}
+                style={{ top: '10%', right: '10%' }}
+              />
             </Plane>
-          </NoLights>
-          <RotateX degrees={-30}>
-            <RotateY degrees={-45}>
-              {/* Floor */}
-              <Floor
-                w={side}
-                h={side}
-                pinX="left"
-                pinY="top"
-                style={{ background: floorColor, overflow: 'hidden' }}
-              >
-                <Slider
-                  value={x}
-                  setValue={setX}
-                  style={{
-                    top: 0,
-                    transform: 'translateY(-50%)',
-                  }}
-                />
-                <Slider
-                  value={z}
-                  setValue={setZ}
-                  style={{
-                    top: 0,
-                    transformOrigin: 'left',
-                    transform: 'translateY(-50%) rotateZ(90deg)',
-                  }}
-                />
-                <ColorPicker
-                  color={floorColor}
-                  setColor={setFloorColor}
-                  style={{ bottom: '10%', right: '10%' }}
-                />
-              </Floor>
-              {/* Left */}
-              <LWall
-                w={side}
-                h={side}
-                pinX="right"
-                pinY="bottom"
-                style={{ background: leftColor, overflow: 'hidden' }}
-              >
-                <Slider
-                  value={z}
-                  setValue={setZ}
-                  style={{
-                    bottom: 0,
-                    transform: 'translateY(50%) rotateZ(180deg)',
-                  }}
-                />
-                <Slider
-                  value={y}
-                  setValue={setY}
-                  style={{
-                    transformOrigin: 'right',
-                    top: 0,
-                    transform: 'translateY(-50%) rotateZ(-90deg)',
-                  }}
-                />
-                <ColorPicker
-                  color={leftColor}
-                  setColor={setLeftColor}
-                  style={{ top: '10%', left: '10%' }}
-                />
-              </LWall>
-              {/* Back */}
-              <Plane
-                w={side}
-                h={side}
-                pinX="left"
-                pinY="bottom"
-                style={{ background: backColor, overflow: 'hidden' }}
-              >
-                <Slider
-                  value={y}
-                  setValue={setY}
-                  style={{
-                    bottom: 0,
-                    transformOrigin: 'left',
-                    transform: 'translateY(50%) rotateZ(-90deg)',
-                  }}
-                />
-                <Slider
-                  value={x}
-                  setValue={setX}
-                  style={{
-                    bottom: 0,
-                    transform: 'translateY(50%) ',
-                  }}
-                />
-                <ColorPicker
-                  color={backColor}
-                  setColor={setBackColor}
-                  style={{ top: '10%', right: '10%' }}
-                />
-              </Plane>
-              <NoLights>
-                <Ball
-                  r={side / 20}
-                  x={x * side}
-                  y={-y * side}
-                  z={z * side}
-                  style={{ background: lightColor }}
-                ></Ball>
-              </NoLights>
-              <PointLight
+            <NoLights>
+              <Ball
+                r={side / 20}
                 x={x * side}
                 y={-y * side}
                 z={z * side}
-                color={lightColor}
-              />
-            </RotateY>
-          </RotateX>
-        </SceneContent>
-      </SceneContainer>
-    </div>
+                style={{ background: lightColor }}
+              ></Ball>
+            </NoLights>
+            <PointLight
+              x={x * side}
+              y={-y * side}
+              z={z * side}
+              color={lightColor}
+            />
+          </RotateY>
+        </RotateX>
+      </SceneContent>
+    </SceneContainer>
   );
 }
 
