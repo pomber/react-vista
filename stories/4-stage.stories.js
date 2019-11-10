8d91a59bf0885cf15efb471681dcf55d3101dafc
Rodrigo Pombo <pombopombopombo@gmail.com>
2019-11-10 19:10:53 -0300

    Change background pattern

--- stories/4-stage.stories.js
+++ stories/4-stage.stories.js
@@ -39,7 +39,8 @@ export const platform = () => (
     </Move>
   </StoryScene>
 );
-
+// const backgroundPattern = `url("data:image/svg+xml,%3Csvg width='64' height='64' viewBox='0 0 64 64' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M8 16c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zm33.414-6l5.95-5.95L45.95.636 40 6.586 34.05.636 32.636 2.05 38.586 8l-5.95 5.95 1.414 1.414L40 9.414l5.95 5.95 1.414-1.414L41.414 8zM40 48c4.418 0 8-3.582 8-8s-3.582-8-8-8-8 3.582-8 8 3.582 8 8 8zm0-2c3.314 0 6-2.686 6-6s-2.686-6-6-6-6 2.686-6 6 2.686 6 6 6zM9.414 40l5.95-5.95-1.414-1.414L8 38.586l-5.95-5.95L.636 34.05 6.586 40l-5.95 5.95 1.414 1.414L8 41.414l5.95 5.95 1.414-1.414L9.414 40z' fill='%23ffffff' fill-opacity='0.4' fill-rule='evenodd'/%3E%3C/svg%3E");`;
+const backgroundPattern = `url("data:image/svg+xml,%3Csvg width='28' height='28' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23999' fill-opacity='0.7' fill-rule='evenodd'%3E%3Ccircle cx='3' cy='3' r='2'/%3E%3Ccircle cx='13' cy='13' r='0'/%3E%3C/g%3E%3C/svg%3E")`;
 const whiteBackground = '#FAF9F5';
 const darkBackground = '#202226';
 
@@ -57,8 +58,8 @@ function StoryScene({ children }) {
         height: '100%',
         width: '100%',
         overflow: 'hidden',
-        color: 'rgb(51,51,51)',
-        background: '#222',
+        background: darkBackground,
+        color: '#222',
       }}
     >
       <SceneContainer
@@ -204,7 +205,7 @@ function Platform() {
         h={6}
         w={width}
         style={{
-          background: '#222',
+          background: '#161616',
           backgroundImage: `url("https://www.transparenttextures.com/patterns/wood.png")`,
         }}
       />
@@ -214,7 +215,7 @@ function Platform() {
         h={1.5}
         w={width}
         style={{
-          background: '#222',
+          background: '#111',
           backgroundImage: `url("https://www.transparenttextures.com/patterns/wood.png")`,
         }}
       />
@@ -232,7 +233,7 @@ function Top() {
         h={6}
         w={width}
         style={{
-          background: '#222',
+          background: '#161616',
           backgroundImage: `url("https://www.transparenttextures.com/patterns/wood.png")`,
         }}
       />
@@ -242,7 +243,7 @@ function Top() {
         h={1}
         w={width}
         style={{
-          background: '#222',
+          background: '#111',
           backgroundImage: `url("https://www.transparenttextures.com/patterns/wood.png")`,
         }}
       />
@@ -283,15 +284,13 @@ function Pulpit() {
 }
 
 function Background() {
-  const scale = useScale();
   return (
     <Plane
       w={14}
       h={10}
       style={{
-        background: '#222',
-        backgroundImage: `url("https://www.transparenttextures.com/patterns/worn-dots.png")`,
-        backgroundSize: scale * 3,
+        background: darkBackground,
+        backgroundImage: backgroundPattern,
       }}
     ></Plane>
   );
